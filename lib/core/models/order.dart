fe81b61a8960cfd080e9ccc5bbcbbe5a2e787866
hybrev <hybrev300@gmail.com>
2024-07-22 22:30:04 +0800

    rework order model

--- lib/core/models/order.dart
+++ lib/core/models/order.dart
@@ -1,33 +1,38 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:hook_diner/core/models/item.dart';
 
 class Order {
-  final int? id;
-  final double totalPrice;
-  final int customerId;
-  final List<Item>? items;
+  String? id;
+  String? name;
+  DocumentReference? customerId;
+  List<Item>? items;
+  double? totalPrice;
+  String? orderStatus;
 
-  const Order({
+  Order({
     this.id,
-    required this.totalPrice,
-    required this.customerId,
+    this.name,
+    this.customerId,
     this.items,
+    this.totalPrice,
+    this.orderStatus,
   });
 
-  factory Order.fromJson(Map<String, dynamic> json) => Order(
-        id: json['id'] as int?,
-        totalPrice: json['total_amount'] as double,
-        customerId: json['customer_id'] as int,
-        items: json['items'] != null
-            ? (json['items'] as List)
-                .map((item) => Item.fromJson(item, item.id))
-                .toList()
-            : null,
+  factory Order.fromJson(Map<String, dynamic> json, String id) => Order(
+        id: json['id'] as String?,
+        name: json['name'] as String?,
+        customerId: json['customer_id'] as DocumentReference?,
+        items: json['items'] as List<Item>?,
+        totalPrice: json['total_price'] as double?,
+        orderStatus: json['order_status'] as String?,
       );
 
   Map<String, dynamic> toJson() => {
         'id': id,
-        'total_amount': totalPrice, // Use snake_case for consistency
+        'name': name,
         'customer_id': customerId,
-        'items': items?.map((item) => item.toJson()).toList(),
+        'items': items,
+        'total_price': totalPrice,
+        'order_status': orderStatus,
       };
 }
